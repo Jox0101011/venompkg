b972e8c86dc1ec815fbf60a2a4b95f041cdb7b15
Jox0101011 <josuemendesviana@gmail.com>
2025-06-09 10:35:18 -0300

    PLUGINS system renoved

--- ext/vsh/plugins/commands.sh
+++ ext/vsh/plugins/commands.sh
@@ -1,10 +1,24 @@
 plugin_name="Commands Basics"
 
 help() {
-	command -f "clear"
+	command clear
+	echo ""
 	echo "Welcome to Vesh-0.2 !"
 	echo ""
 	echo ""
 	echo ""
-	echo ""
+	echo "by: jox0101011/emperor"
+}
+
+help_plugin() {
+	echo "Vesh-0.2 !"
+	echo "For add plugins:"
+	echo "1- create your plugin using example base"
+	echo "2- add in git"
+	echo "3- clone to the path ext/vsh/plugins/your_plugin.sh"
+	echo "4- use commands for test ;)"
+}
+
+clear() {
+	command clear
 }
